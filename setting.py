e107dcc1946de615af2ea7530d926874f4c92c71
ao213 <61234482+ao213@users.noreply.github.com>
2021-07-22 19:58:21 +0900

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,25 @@
+#画像パス
+LOGO_PATH = 'img/fruit_apple.png'
+BG_PATH = 'img/apple_bg.jpg'
+HUD_BG_PATH = 'img/hud.png'
+GAMEOVER_BG_PATH = 'img/GAME OVER.png'
+
+
+#画面設定#
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 800
+CAPTION = 'Apple_Catch_Game'
+
+
+#apple_setting#
+APPLE_JSON = 'apple.json'
+MAX_APPLE_NUM = 10
+
+
+#player_setting#
+MAX_HP = 100
+
+#HUD設定#
+LINE_X = 873
+HUD_TEXT_COLOR = (127,195,156)
+    
